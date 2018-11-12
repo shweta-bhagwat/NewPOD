7d342aaa24f61adcec89eece4b45f56f4440e6b1
shweta.bhagwat <shweta.bhagwat@t-mobile.com>
2018-11-11 18:13:26 -0800

    Ffinished framework

---
+++ NewPOD/NewPOD/NewPOD.h
@@ -0,0 +1,19 @@
+//
+//  NewPOD.h
+//  NewPOD
+//
+//  Created by shweta.bhagwat on 11/11/18.
+//  Copyright © 2018 shweta.bhagwat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NewPOD.
+FOUNDATION_EXPORT double NewPODVersionNumber;
+
+//! Project version string for NewPOD.
+FOUNDATION_EXPORT const unsigned char NewPODVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NewPOD/PublicHeader.h>
+
+
---
+++ NewPOD/NewPOD/Service.swift
@@ -0,0 +1,18 @@
+//
+//  Service.swift
+//  NewPOD
+//
+//  Created by shweta.bhagwat on 11/11/18.
+//  Copyright © 2018 shweta.bhagwat. All rights reserved.
+//
+
+import Foundation
+
+public class Service {
+    
+    private init () {}
+    
+    public static func try1 () -> String {
+        return "Hello Service!"
+    }
+}
